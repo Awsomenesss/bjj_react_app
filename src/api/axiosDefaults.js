e619c5b39873b061927c27040a4e98f6b8f24b76
Awsomenesss <>
2023-08-31 08:07:42 +0000

    applied the basic functonality in the navbar

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,7 @@ import axios from "axios";
 axios.defaults.baseURL= 'https://pp5-bjj-api-2269f4220822.herokuapp.com/'
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+// Axios request with the above default custom values
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
